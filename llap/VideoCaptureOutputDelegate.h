65434be24901f8dc7955b37f4626172a3406e3de
chentong <chentong@chentongdeiMac.local>
2020-07-03 09:18:11 +0800

    first commit

---
+++ llap/VideoCaptureOutputDelegate.h
@@ -0,0 +1,13 @@
+//
+//  VideoCaptureOutputDelegate.h
+//  llap
+//
+//  Created by chentong on 2020/7/2.
+//  Copyright © 2020年 Nanjing University. All rights reserved.
+//
+
+#ifndef VideoCaptureOutputDelegate_h
+#define VideoCaptureOutputDelegate_h
+
+
+#endif /* VideoCaptureOutputDelegate_h */
---
+++ llap/VideoCaptureOutputDelegate.m
@@ -0,0 +1,9 @@
+//
+//  VideoCaptureOutputDelegate.m
+//  llap
+//
+//  Created by chentong on 2020/7/2.
+//  Copyright © 2020年 Nanjing University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
